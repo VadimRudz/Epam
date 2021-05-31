849050924109963ad3a1b66a4a0423c911596ef5
Vadim Rudz <Vadimrudz.gmail.com>
2021-05-31 14:24:08 +0200

    6th commit : add Main class

---
+++ toGit(Epam)/src/javaClasses_MainTask/MainTask_Main.java
@@ -0,0 +1,24 @@
+package javaClasses_MainTask;
+
+public class MainTask_Main {
+
+	public static void main(String[] args) {
+		
+		Student stdnt = new Student();
+		
+		stdnt.arrayStudents();
+    	
+		
+		Customer cstmr = new Customer();
+		
+    	cstmr.arrayCustomers();
+		
+		
+		Patient ptnt = new Patient();
+		
+		ptnt.arrayPatients();
+			
+
+	}
+
+}
