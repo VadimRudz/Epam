46828a4c7655608f5ac77279dae2690718105be8
VadimRudz <83311092+VadimRudz@users.noreply.github.com>
2021-06-06 22:38:43 +0300

    Update Patient.java

--- toGit(Epam)/src/javaClasses_MainTask/Patient.java
+++ toGit(Epam)/src/javaClasses_MainTask/Patient.java
@@ -6,18 +6,17 @@ public class Patient extends Human{
 	private int telephone;
 	private int medicalCardNumber;
 	private String diagnosis;
+	Patient [] samplePatient = new Patient [5];
 	
 	public Patient(){	
 		super();	
 	}
 	
 	public Patient(int id, String surname, String name, String patronymic, String address, int telephone,  int medicalCardNumber, String diagnosis){
-		
 		super (id, surname, name, patronymic, address);
 		this.telephone = telephone;
 		this.medicalCardNumber = medicalCardNumber;
 		this.diagnosis = diagnosis;
-		
 	}
 	
 	public Patient(int id, String surname,int medicalCardNumber, String diagnosis)
@@ -27,26 +26,16 @@ public class Patient extends Human{
 	
 	@Override
 	public String toString() { 
-		
-        return "Ïğèâåò, ÿ ïàöèåíò: Id - " + getId() + ", ôàìèëèÿ - " + getSurname() + ", èìÿ - " + getName() + ", îò÷åñòâî - " + getPatronymic() + ", àäğåññ- " + getAddress() + ", " 
-        		+ " òåëåôîí - " + getTelephone() + ", íîìåğ ìåäèöèíñêîé êàğòû - " + getMedicalCardNumber() + ", äèàãíîç - " + getDiagnosis() + ";";
+        return "ĞŸÑ€Ğ¸Ğ²ĞµÑ‚, Ñ Ğ¿Ğ°Ñ†Ğ¸ĞµĞ½Ñ‚: Id - " + getId() + ", Ñ„Ğ°Ğ¼Ğ¸Ğ»Ğ¸Ñ - " + getSurname() + ", Ğ¸Ğ¼Ñ - " + getName() + ", Ğ¾Ñ‚Ñ‡ĞµÑÑ‚Ğ²Ğ¾ - " + getPatronymic() + ", Ğ°Ğ´Ñ€ĞµÑÑ- " + getAddress() + ", " 
+        		+ " Ñ‚ĞµĞ»ĞµÑ„Ğ¾Ğ½ - " + getTelephone() + ", Ğ½Ğ¾Ğ¼ĞµÑ€ Ğ¼ĞµĞ´Ğ¸Ñ†Ğ¸Ğ½ÑĞºĞ¾Ğ¹ ĞºĞ°Ñ€Ñ‚Ñ‹ - " + getMedicalCardNumber() + ", Ğ´Ğ¸Ğ°Ğ³Ğ½Ğ¾Ğ· - " + getDiagnosis() + ";";
      } 
 	
 	public void arrayObjects() { 
-		
-		Patient [] samplePatient = new Patient [5];
-		samplePatient [0]  = new Patient (15, "Ïàğêåğ", 1547, "ñòğåññ");
-		samplePatient [1]  = new Patient (16, "Áıííåğ", 1574, "ñòğåññ");
-		samplePatient [2]  = new Patient (17, "Ñòğıíäæ", 1637, "ñòğåññ");
-		samplePatient [3]  = new Patient (18, "Ñàììåğñ", 1333, "ñòğåññ");
-		samplePatient [4]  = new Patient (19, "Îêòàâèóñ", 1987, "ñòğåññ");
-		
-		for ( Patient ptnt : samplePatient) {
-			
-			if (ptnt.getMedicalCardNumber()==1547 && ptnt.getDiagnosis().equals("ñòğåññ")) {
-				System.out.println("ß - " + ptnt.getSurname() + ", ìîé íîìåğ ìåäèöèíñêîé êàğòû - "  + ptnt.getMedicalCardNumber() + ", äèàãíîç -" + ptnt.getDiagnosis()+ ";");
-			}
-		  }
+		samplePatient [0]  = new Patient (15, "ĞŸĞ°Ñ€ĞºĞµÑ€", 1547, "Ğ´ĞµĞ¿Ñ€ĞµÑÑĞ¸Ñ");
+		samplePatient [1]  = new Patient (16, "Ğ‘ÑĞ½Ğ½ĞµÑ€", 1574, "ÑÑ‚Ñ€ĞµÑÑ");
+		samplePatient [2]  = new Patient (17, "Ğ¡Ñ‚Ñ€ÑĞ½Ğ´Ğ¶", 1637, "Ñ€Ğ°ÑÑ‚Ñ€Ğ¾Ğ¹ÑÑ‚Ğ²Ğ¾ Ğ»Ğ¸Ñ‡Ğ½Ğ¾ÑÑ‚Ğ¸");
+		samplePatient [3]  = new Patient (18, "Ğ¡Ğ°Ğ¼Ğ¼ĞµÑ€Ñ", 1333, "ÑÑ‚Ñ€ĞµÑÑ");
+		samplePatient [4]  = new Patient (19, "ĞĞºÑ‚Ğ°Ğ²Ğ¸ÑƒÑ", 1987, "Ğ¿Ñ€Ğ¾Ñ„Ñ„ĞµÑĞ¸Ğ°Ğ½Ğ°Ğ»ÑŒĞ½Ğ¾Ğµ Ğ²Ñ‹Ğ³Ğ¾Ñ€Ğ°Ğ½Ğ¸Ğµ");
 	}
 	
 	public int getTelephone() {
@@ -73,4 +62,23 @@ public class Patient extends Human{
         this.diagnosis = diagnosis;
     }
     
+    public void sortDiagnosis(Patient [] samplePatient, String diagnosis){
+    	System.out.println("CĞ¿Ğ¸ÑĞ¾Ğº Ğ¿Ğ°Ñ†Ğ¸ĞµĞ½Ñ‚Ğ¾Ğ² Ñ Ğ´Ğ¸Ğ°Ğ³Ğ½Ğ¾Ğ·Ğ¾Ğ¼ " + diagnosis + ":");
+    	for(Patient patient: samplePatient) {
+    		if(patient.getDiagnosis().equals(diagnosis)){
+    			System.out.print(patient.getSurname()+", ");
+    		}
+    	}
+    	System.out.println();
+    }
+    
+    public void medicalCardNumberInterval(Patient [] samplePatient, int numberMin, int numberMax){
+    	System.out.println("Ğ¡Ğ¿Ğ¸ÑĞ¾Ğº Ğ¿Ğ°Ñ†Ğ¸ĞµĞ½Ñ‚Ğ¾Ğ², Ğ¾Ñ‚ÑĞ¾Ñ€Ñ‚Ğ¸Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ½Ñ‹Ñ… Ğ¿Ğ¾ Ğ½Ğ¾Ğ¼ĞµÑ€Ñƒ Ğ¼ĞµĞ´Ğ¸Ñ†Ğ¸Ğ½ÑĞºĞ¾Ğ¹ ĞºĞ°Ñ€Ñ‚Ñ‹:");
+    	for(Patient patient : samplePatient) {
+    		if(patient.getMedicalCardNumber()>numberMin && patient.getMedicalCardNumber()<numberMax){
+    			System.out.print(patient.getSurname()+", ");
+    		}
+    	}
+    	System.out.println();
+    }
 }
