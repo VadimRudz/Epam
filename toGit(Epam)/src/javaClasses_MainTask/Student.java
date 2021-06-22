e7c39caa3dd9bc44f93a648f5e5f52ae1710b088
VadimRudz <83311092+VadimRudz@users.noreply.github.com>
2021-06-22 20:14:44 +0300

    Update Student.java

--- toGit(Epam)/src/javaClasses_MainTask/Student.java
+++ toGit(Epam)/src/javaClasses_MainTask/Student.java
@@ -40,11 +40,11 @@ public class Student extends Human{
 	
 	@Override
 	public String toString() { 
-        return "Привет, я - студент: Id - " + getId() + ", фамилия - " + getSurname() 
-        + ", имя - " + getName() + ", отчество - " + getPatronymic() + ", адресс- " 
-        + getAddress() + ", " + "дата рождения - " + getDateOfBirth() + ", телефон - " 
-        + getTelephone() + ", факультет - " + getFaculty() + ", курс - " + getCourse() 
-        + ", группа - " + getGroup() + ";";
+        return "РџСЂРёРІРµС‚, СЏ - СЃС‚СѓРґРµРЅС‚: Id - " + getId() + ", С„Р°РјРёР»РёСЏ - " + getSurname() 
+        + ", РёРјСЏ - " + getName() + ", РѕС‚С‡РµСЃС‚РІРѕ - " + getPatronymic() + ", Р°РґСЂРµСЃСЃ- " 
+        + getAddress() + ", " + "РґР°С‚Р° СЂРѕР¶РґРµРЅРёСЏ - " + getDateOfBirth() + ", С‚РµР»РµС„РѕРЅ - " 
+        + getTelephone() + ", С„Р°РєСѓР»СЊС‚РµС‚ - " + getFaculty() + ", РєСѓСЂСЃ - " + getCourse() 
+        + ", РіСЂСѓРїРїР° - " + getGroup() + ";";
      } 
 	
 	public GregorianCalendar getDateOfBirth() {
@@ -88,18 +88,18 @@ public class Student extends Human{
     }
 
 	public void arrayObjects() {
-		sampleStudent[0] = new Student(1, "Михеев", "Дмитрий", "Валерьевич", "Проспект строителей, 18", new GregorianCalendar(1987, Calendar.FEBRUARY , 23), 297896614, "Транспортных коммуникаций", 5, 114315);
-		sampleStudent[1] = new Student(2, "Бонадысев", "Дмитрий", "Александрович", "Проспект победителей, 15", new GregorianCalendar(1988, Calendar.NOVEMBER , 27), 33389156, "Транспортных коммуникаций", 3, 114315);
-		sampleStudent[2] = new Student(3, "Климович", "Александр", "Сергееевич", "Якуба Колоса, 12", new GregorianCalendar(1986, Calendar.JANUARY , 18), 335569147, "Транспортных коммуникаций", 4, 114335);
-		sampleStudent[3] = new Student(4, "Евменова", "Дарья", "Александровна", "Янки Купалы, 2", new GregorianCalendar(1990, Calendar.APRIL , 3), 297896543, "Транспортных коммуникаций", 2, 114335);
-		sampleStudent[4] = new Student(5, "Билоус", "Виталий", "Дмитриевич", "Мавра, 87", new GregorianCalendar(1991, Calendar.JULY , 15), 292345678, "Транспортных коммуникаций", 1, 114345);
-		sampleStudent[5] = new Student(6, "Аксамит", "Виталий", "Вадимович", "Лидская, 33", new GregorianCalendar(1985, Calendar.AUGUST , 05), 292466378, "Технологий управления и гуманитаризации", 3, 114345);
-		sampleStudent[6] = new Student(7, "Вроцлав", "Василий", "Викторович", "Московская, 12", new GregorianCalendar(1987, Calendar.AUGUST , 21), 292345466, "Информационных технологий", 1, 114125);
-		sampleStudent[7] = new Student(8, "Ивулин", "Александр", "Дмитриевич", "Октябрьская, 1", new GregorianCalendar(1995, Calendar.DECEMBER, 1), 292366545, "Энергоснабжения", 1, 114125);
+		sampleStudent[0] = new Student(1, "РњРёС…РµРµРІ", "Р”РјРёС‚СЂРёР№", "Р’Р°Р»РµСЂСЊРµРІРёС‡", "РџСЂРѕСЃРїРµРєС‚ СЃС‚СЂРѕРёС‚РµР»РµР№, 18", new GregorianCalendar(1987, Calendar.FEBRUARY , 23), 297896614, "РўСЂР°РЅСЃРїРѕСЂС‚РЅС‹С… РєРѕРјРјСѓРЅРёРєР°С†РёР№", 5, 114315);
+		sampleStudent[1] = new Student(2, "Р‘РѕРЅР°РґС‹СЃРµРІ", "Р”РјРёС‚СЂРёР№", "РђР»РµРєСЃР°РЅРґСЂРѕРІРёС‡", "РџСЂРѕСЃРїРµРєС‚ РїРѕР±РµРґРёС‚РµР»РµР№, 15", new GregorianCalendar(1988, Calendar.NOVEMBER , 27), 33389156, "РўСЂР°РЅСЃРїРѕСЂС‚РЅС‹С… РєРѕРјРјСѓРЅРёРєР°С†РёР№", 3, 114315);
+		sampleStudent[2] = new Student(3, "РљР»РёРјРѕРІРёС‡", "РђР»РµРєСЃР°РЅРґСЂ", "РЎРµСЂРіРµРµРµРІРёС‡", "РЇРєСѓР±Р° РљРѕР»РѕСЃР°, 12", new GregorianCalendar(1986, Calendar.JANUARY , 18), 335569147, "РўСЂР°РЅСЃРїРѕСЂС‚РЅС‹С… РєРѕРјРјСѓРЅРёРєР°С†РёР№", 4, 114335);
+		sampleStudent[3] = new Student(4, "Р•РІРјРµРЅРѕРІР°", "Р”Р°СЂСЊСЏ", "РђР»РµРєСЃР°РЅРґСЂРѕРІРЅР°", "РЇРЅРєРё РљСѓРїР°Р»С‹, 2", new GregorianCalendar(1990, Calendar.APRIL , 3), 297896543, "РўСЂР°РЅСЃРїРѕСЂС‚РЅС‹С… РєРѕРјРјСѓРЅРёРєР°С†РёР№", 2, 114335);
+		sampleStudent[4] = new Student(5, "Р‘РёР»РѕСѓСЃ", "Р’РёС‚Р°Р»РёР№", "Р”РјРёС‚СЂРёРµРІРёС‡", "РњР°РІСЂР°, 87", new GregorianCalendar(1991, Calendar.JULY , 15), 292345678, "РўСЂР°РЅСЃРїРѕСЂС‚РЅС‹С… РєРѕРјРјСѓРЅРёРєР°С†РёР№", 1, 114345);
+		sampleStudent[5] = new Student(6, "РђРєСЃР°РјРёС‚", "Р’РёС‚Р°Р»РёР№", "Р’Р°РґРёРјРѕРІРёС‡", "Р›РёРґСЃРєР°СЏ, 33", new GregorianCalendar(1985, Calendar.AUGUST , 05), 292466378, "РўРµС…РЅРѕР»РѕРіРёР№ СѓРїСЂР°РІР»РµРЅРёСЏ Рё РіСѓРјР°РЅРёС‚Р°СЂРёР·Р°С†РёРё", 3, 114345);
+		sampleStudent[6] = new Student(7, "Р’СЂРѕС†Р»Р°РІ", "Р’Р°СЃРёР»РёР№", "Р’РёРєС‚РѕСЂРѕРІРёС‡", "РњРѕСЃРєРѕРІСЃРєР°СЏ, 12", new GregorianCalendar(1987, Calendar.AUGUST , 21), 292345466, "РРЅС„РѕСЂРјР°С†РёРѕРЅРЅС‹С… С‚РµС…РЅРѕР»РѕРіРёР№", 1, 114125);
+		sampleStudent[7] = new Student(8, "РРІСѓР»РёРЅ", "РђР»РµРєСЃР°РЅРґСЂ", "Р”РјРёС‚СЂРёРµРІРёС‡", "РћРєС‚СЏР±СЂСЊСЃРєР°СЏ, 1", new GregorianCalendar(1995, Calendar.DECEMBER, 1), 292366545, "Р­РЅРµСЂРіРѕСЃРЅР°Р±Р¶РµРЅРёСЏ", 1, 114125);
 	}
 	
-	public void outputStudentsSortedByFaculty (Student [] sampleStudent, String faculty) { //метод выводит всех студентов одного факультета
-		System.out.println("Список студентов факультета " + faculty + ":");
+	public void outputStudentsSortedByFaculty (Student [] sampleStudent, String faculty) { //РјРµС‚РѕРґ РІС‹РІРѕРґРёС‚ РІСЃРµС… СЃС‚СѓРґРµРЅС‚РѕРІ РѕРґРЅРѕРіРѕ С„Р°РєСѓР»СЊС‚РµС‚Р°
+		System.out.println("РЎРїРёСЃРѕРє СЃС‚СѓРґРµРЅС‚РѕРІ С„Р°РєСѓР»СЊС‚РµС‚Р° " + faculty + ":");
 		for ( Student stdnt : sampleStudent) {
 			newHashSetFaculty.add(stdnt.getFaculty());
 			newHashSetGroup.add(stdnt.getGroup());
@@ -110,30 +110,30 @@ public class Student extends Human{
 		System.out.print("");
 	}
 	
-	public void outputStudentsSortedByFacultyAndCourse (Student [] sampleStudent){  // метод выводит всех студентов, скомпанованных по факультету 
+	public void outputStudentsSortedByFacultyAndCourse (Student [] sampleStudent){  // РјРµС‚РѕРґ РІС‹РІРѕРґРёС‚ РІСЃРµС… СЃС‚СѓРґРµРЅС‚РѕРІ, СЃРєРѕРјРїР°РЅРѕРІР°РЅРЅС‹С… РїРѕ С„Р°РєСѓР»СЊС‚РµС‚Сѓ 
 		
 		for (String strng : newHashSetFaculty) {
-			System.out.println("Список студентов факультета " + strng + ": ");
+			System.out.println("РЎРїРёСЃРѕРє СЃС‚СѓРґРµРЅС‚РѕРІ С„Р°РєСѓР»СЊС‚РµС‚Р° " + strng + ": ");
 			
 			for ( Student stdnt : sampleStudent) {
 				if (stdnt.getFaculty().equalsIgnoreCase(strng)) {
-					System.out.println(stdnt.getSurname() + ", курс - " + stdnt.getCourse());
+					System.out.println(stdnt.getSurname() + ", РєСѓСЂСЃ - " + stdnt.getCourse());
 				}
 			}
 		}
 	}
 	
-	public void outputStudentsSortedByDateOfBirth(Student [] sampleStudent, int year){ // метод выводит студентов, родившихся после 1990 года
+	public void outputStudentsSortedByDateOfBirth(Student [] sampleStudent, int year){ // РјРµС‚РѕРґ РІС‹РІРѕРґРёС‚ СЃС‚СѓРґРµРЅС‚РѕРІ, СЂРѕРґРёРІС€РёС…СЃСЏ РїРѕСЃР»Рµ 1990 РіРѕРґР°
 		for (Student stdnt : sampleStudent) { 
 			if (stdnt.getDateOfBirth().get(GregorianCalendar.YEAR) > year) {
-				System.out.println(stdnt.getSurname() + ", год рождения - " + stdnt.getDateOfBirth().get(GregorianCalendar.YEAR));
+				System.out.println(stdnt.getSurname() + ", РіРѕРґ СЂРѕР¶РґРµРЅРёСЏ - " + stdnt.getDateOfBirth().get(GregorianCalendar.YEAR));
 			}
 		}
 	}
 	
-	public void outputStudentsSortedByGroup (Student [] sampleStudent) {// метод выводит всех студентов, скомпанованных по группам
+	public void outputStudentsSortedByGroup (Student [] sampleStudent) {// РјРµС‚РѕРґ РІС‹РІРѕРґРёС‚ РІСЃРµС… СЃС‚СѓРґРµРЅС‚РѕРІ, СЃРєРѕРјРїР°РЅРѕРІР°РЅРЅС‹С… РїРѕ РіСЂСѓРїРїР°Рј
 		for (int score : newHashSetGroup) {
-			System.out.println("Список студентов группы " + score + ": ");
+			System.out.println("РЎРїРёСЃРѕРє СЃС‚СѓРґРµРЅС‚РѕРІ РіСЂСѓРїРїС‹ " + score + ": ");
 			
 			for ( Student stdnt : sampleStudent) {
 				if (stdnt.getGroup() == score) {
